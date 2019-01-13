ab6594a3aa8d1a39911bff4dc49b1f581ffce12d
bssm1989 <bssm1989@gamil.com>
2019-01-13 15:25:26 +0700

    Add project files.

---
+++ WebApplication19/Controllers/studentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        santisar_Entities db =new santisar_Entities();
+        public ActionResult Index()
+        {
+            return View(db.STUDENTS.ToList());
+        }
+    }
+}
